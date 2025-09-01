f0fa00ffdad0d931d54f404bdda82a804ae13ada
aashirwadinstitute1-ctrl <aashirwadinstitute1@gmail.com>
2025-09-01 18:10:41 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,20 @@
+const questions = [
+  {
+    id: 1,
+    text: "Calendar में 2025 का पहला दिन कौन सा होगा?",
+    options: ["Monday", "Wednesday", "Thursday", "Saturday"],
+    answer: 1
+  },
+  {
+    id: 2,
+    text: "Dice में 1 के सामने कौन सा number होता है?",
+    options: ["2", "3", "5", "6"],
+    answer: 3
+  },
+  {
+    id: 3,
+    text: "Mirror image of 'PQRS' क्या होगा?",
+    options: ["SRQP", "SPQR", "RSPQ", "QPSR"],
+    answer: 0
+  }
+];
